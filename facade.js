6f803b737aec776fa8ddc520610799ba291274af
fullofheal <zelenskiymisha@gmail.com>
2023-02-10 13:50:30 +0200

    facade updated

--- facade.js
+++ facade.js
@@ -11,26 +11,49 @@
 // ```
 // Implement class that will provide simple interface with one method which allows apply resume for each company and get the result.
 
+const resumeExample = {
+  qualification: true,
+  meetsRequirements: false,
+  meetsSalaryExpectations: true  
+}
+
 class CompanyA {
-  checkResume() {}
+  checkResume(qualification) {
+    return qualification ? true : false
+  }
 }
 
 class CompanyB {
-  checkRequirements() {}
+  checkRequirements(meetsRequirements) {
+    return meetsRequirements ? true : false
+  }
 }
 
 class CompanyC {
-  checkSalaryExpectations() {}
+  checkSalaryExpectations(meetsSalaryExpectations) {
+    return meetsSalaryExpectations ? true : false
+  }
 }
 
 class ApplyResume {
   #companyA = new CompanyA;
   #companyB = new CompanyB;
   #companyC = new CompanyC;
+  constructor(resume) {
+    this.qualification = resume.qualification,
+    this.meetsRequirements = resume.meetsRequirements,
+    this.meetsSalaryExpectations = resume.meetsSalaryExpectations
+  }
   
   apply() {
-    this.#companyA.checkResume();
-    this.#companyB.checkRequirements();
-    this.#companyC.checkSalaryExpectations();
+    const qualified = this.#companyA.checkResume(this.qualification);
+    const requrementsMet = this.#companyB.checkRequirements(this.meetsRequirements);
+    const salaryExpectationsMet = this.#companyC.checkSalaryExpectations(this.meetsSalaryExpectations);
+
+    return qualified && requrementsMet && salaryExpectationsMet ? 'Applicant is suitable for this position' : 'Applicant doesnt match the position'
   }
 }
+
+const application = new ApplyResume(resumeExample);
+
+console.log(application.apply());
